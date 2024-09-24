6067ae8e5679e596a9773be68b1132d20193cd63
Soshi Tsunashima <22724971+hellososhi@users.noreply.github.com>
2024-09-24 15:22:54 +0900

    [add] button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,3 +1,29 @@
 import { Button as BaseButton } from "@mui/base";
+import { styled } from "@pigment-css/react";
 
-export const Button = BaseButton;
+const StyledButton = styled(BaseButton)(() => ({
+  backgroundColor: "blue",
+  color: "white",
+  padding: "10px 20px",
+  border: "none",
+  borderRadius: "5px",
+  cursor: "pointer",
+  "&:disabled": {
+    backgroundColor: "gray",
+    cursor: "not-allowed",
+  },
+}));
+
+export const Button = ({ children, disabled, onClick }: ButtonProps) => {
+  return (
+    <StyledButton disabled={disabled} onClick={onClick}>
+      {children}
+    </StyledButton>
+  );
+};
+
+export type ButtonProps = {
+  children: string;
+  onClick: () => void;
+  disabled?: boolean;
+};
