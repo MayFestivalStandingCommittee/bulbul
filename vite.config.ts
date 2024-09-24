e3ff8d3920f5f32b7d343155b91f0010ebf26745
Soshi Tsunashima <22724971+hellososhi@users.noreply.github.com>
2024-09-24 15:05:46 +0900

    [fix] pigment-css を使えるようにする

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 import { pigment } from "@pigment-css/vite-plugin";
 
 const pigmentConfig = {
-  transformLibraries: ['@mui/material'],
+  transformLibraries: ["@mui/base"],
 };
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // plugins: [react()],
   plugins: [react(), pigment(pigmentConfig)],
   build: {
-    outDir: 'dist', // default の設定と同じ
+    outDir: "dist", // default の設定と同じ
     lib: {
-      entry: 'src/index.ts',
-      name: 'bulbul',
-      fileName: 'index',
-      formats: ['es', 'umd'], // default の設定と同じ
+      entry: "src/index.ts",
+      name: "bulbul",
+      fileName: "index",
+      formats: ["es", "umd"], // default の設定と同じ
     },
     rollupOptions: {
-      external: ['react', 'react-dom'],
+      external: ["react", "react-dom"],
       output: {
         globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
+          react: "React",
+          "react-dom": "ReactDOM",
         },
       },
     },
   },
-})
+});
